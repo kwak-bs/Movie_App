2e3c960fd31ba9bf936d3ec0c97456ede175b32f
kwak-bs <iam-bs@naver.com>
2021-01-10 16:26:48 +0900

    FEAT : 네비게이션 추가

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navigation.css";
 
+// a href 는 html이므로 홈페이지를 새로 고침 시킴
+// Link는 새로고침 안시킴
 function Navigation() {
   return (
     <div className="nav">
