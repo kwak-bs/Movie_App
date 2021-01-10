536de38a816a390e567c76dd3e7dd404f084eb5f
kwak-bs <iam-bs@naver.com>
2021-01-10 15:38:54 +0900

    DOCS : components 폴더 생성

---
+++ src/components/Movie.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+function Movie({ year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__date">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
+      </div>
+    </div>
+  );
+}
+
+// props 타입 정의
+// propsTypes는 number, string, bool, func, node등 다양하게 존재한다.
+Movie.propTypes = {
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+// export를 해줘야 다른 js에서 Movie라는 Componenet를 사용할 수 있다.
+export default Movie;
