6d9869bbb711cd454a0467aa5ec6f11001f56c9b
kwak-bs <iam-bs@naver.com>
2021-01-10 17:25:43 +0900

    ADD : 주석 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,28 @@
 import React from "react";
 
-function Detail(props) {
-  console.log(props);
-  return <span>hello</span>;
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+
+    // 클릭해서 들어오는 것이 아닌 검색해서 들어온다면
+    if (location.state === undefined) {
+      // home으로 redirect
+      history.push("/");
+    }
+  }
+  //componentDidmount()는 class component내에서
+  //render가 실행되기 전에(mount 되기전에) 실행됨
+
+  render() {
+    const { location } = this.props;
+    // 클릭해서 들어오면 잘 보여주고
+    // 새로고침하면 다시 redirect
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
