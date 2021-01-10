8b3ffe6eeeac01dd9f2b7ca971bd7aa94559fc30
kwak-bs <iam-bs@naver.com>
2021-01-10 16:53:31 +0900

    MOD : path 수정 /movie/:id

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 
 //네비게이션은 라우터 안에 위치하고 있어야됨
@@ -13,6 +14,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
