788d6c4894ec162c700fc11e0dc29f4d8b8ff29f
kwak-bs <iam-bs@naver.com>
2021-01-10 16:26:02 +0900

    ADD : 주석 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
-import Navigation from "./components/Navigation"
+import Navigation from "./components/Navigation";
 
+//네비게이션은 라우터 안에 위치하고 있어야됨
+// HashRouter가 github page에 좀 더 잘 연동됨.
+// BrowserRouter는 github page에 설정하기 좀 짜증남.
 function App() {
   return (
     <HashRouter>
-      <Navigation/>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
